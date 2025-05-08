d76dfa539b87358937737f7da6b41e61a1eb391e
Julianeves01 <julia.s.neves6@aluno.senai.br>
2025-05-08 11:53:25 -0300

    gif adicionado

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -6,24 +6,14 @@ import styles from "../../styles/Home.module.css";
 export default function Home() {
     return (
         <div className={styles.container}>
-            <Image className={styles.image} src={"/images/maiko.png"} width={300} height={300} alt="Maiko Xikixiki" />
-            <h1 className={styles.title}> Maiko Xikixiki Bahia</h1>
-            <div className={styles.description}>
-                <p>Tá perdido no código? Relaxa e vem de Maiko, que vamos te mostrar a usar:</p>
-                <ul className={styles.list}>
-                    <li>Next.js (App Router)</li>
-                    <li>CSS Modules</li>
-                    <li>React Components</li>
-                    <li>React Hooks</li>
-                    <li>PreLoad</li>
-                    <li>PreFetch</li>
-                    <li>Fetch Axios</li>
-                    <li>LocalStorage</li>
-                    <li>React Toastify</li>
-                    <li>AntD</li>
-                    <li>Skeleton</li>
-                </ul>
-            </div>
+            <Image
+                className={styles.image}
+                src="/images/planeta-rosa.gif"
+                width={400}
+                height={400}
+                alt="Mundo Rosa Girando"
+            />
+            <h1 className={styles.title}>Bem-vindo ao Mundo dos Países</h1>
             <Link href="/countries">
                 <button className={styles.button}>Acessar Países</button>
             </Link>
